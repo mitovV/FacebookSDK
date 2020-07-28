348ff5151dc539c30a244155e810b0477607221e
CodingMania <vasilmitovv@gmail.com>
2020-07-28 09:54:55 +0300

    Implement models and data parser

---
+++ FacebookSDK/DataParser/Parser.cs
@@ -0,0 +1,58 @@
+﻿namespace DataParser
+{
+    using System.Net.Http;
+
+    using AngleSharp.Html.Parser;
+
+    public class Parser
+    {
+        public PostDTO GetData(string baseUrl)
+        {
+            var parser = new HtmlParser();
+            var httpClient = new HttpClient();
+
+            httpClient.DefaultRequestHeaders
+                .Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0");
+
+            var html = httpClient
+                .GetStringAsync(baseUrl)
+                .GetAwaiter()
+                .GetResult();
+
+            var document = parser
+                .ParseDocument(html);
+
+            var element = document
+                .QuerySelector("div[class='moduletable'] div[class='vmgroup'] ul li");
+
+            var productDetailsLink = baseUrl + element
+                .FirstElementChild
+                .Attributes["href"]
+                .Value
+                .Trim();
+
+            var pictureUrl = baseUrl + element
+                .FirstElementChild
+                .QuerySelector("img")
+                .Attributes["src"]
+                .Value;
+
+            var title = element
+                .QuerySelector("div[class='clear']")
+                .NextElementSibling
+                .InnerHtml;
+
+            var price = document
+                .QuerySelector("div[class='moduletable'] div[class='vmgroup'] div[class='product-price'] div [class='PricesalesPrice']")
+                .InnerHtml;
+
+            return new PostDTO
+            {
+                ProductDetailsLink = productDetailsLink,
+                PictureUrl = pictureUrl,
+                Title = title,
+                Price = price
+            };
+        }
+    }
+}
---
+++ FacebookSDK/Models/PostDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace DataParser
+{
+    public class PostDTO
+    {
+        public string ProductDetailsLink { get; set; }
+
+        public string PictureUrl { get; set; }
+
+        public string Title { get; set; }
+
+        public string Price { get; set; }
+    }
+}
